1c75d83c598821b140234c3b148c466b86727b3a
Artur Mnatsakanyan <Turafc16@gmail.com>
2025-03-27 00:30:15 +0300

    Version 2.0 (Full adaptive mobile and Burger-menu)

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 window.onload = function () {
 	window.scrollTo(0, 0)
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('burger').addEventListener('click', function () {
+		document.querySelector('header').classList.toggle('open')
+	})
+})
